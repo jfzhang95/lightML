819f69ae118f691b80920cd43280538e3f8876ee
jfzhang95 <jfzhang@whu.edu.cn>
2017-08-31 13:48:00 +0800

    add NaiveBayes

--- SupervisedLearning/NaiveBayes.py
+++ SupervisedLearning/NaiveBayes.py
@@ -13,4 +13,63 @@ class NaiveBayes(object):
     def __init__(self):
         pass
 
-    # def
+    def setOfWordsToVecTor(self, vocabularyList, smsWords):
+        """
+        SMS内容匹配预料库，标记预料库的词汇出现的次数
+        :param vocabularyList:
+        :param smsWords:
+        :return:
+        """
+        vocabMarked = [0] * len(vocabularyList)
+        for smsWord in smsWords:
+            if smsWord in vocabularyList:
+                vocabMarked[vocabularyList.index(smsWord)] += 1
+        return vocabMarked
+
+    def setOfWordsListToVecTor(self, vocabularyList, smsWordsList):
+        """
+        将文本数据的二维数组标记
+        :param vocabularyList:
+        :param smsWordsList:
+        :return:
+        """
+        vocabMarkedList = []
+        for i in range(len(smsWordsList)):
+            vocabMarked = self.setOfWordsToVecTor(vocabularyList, smsWordsList[i])
+            vocabMarkedList.append(vocabMarked)
+        return vocabMarkedList
+
+    def fit(self, trainMarkedWords, trainCategory):
+        numTrainDoc = len(trainMarkedWords)
+        numWords = len(trainMarkedWords[0])
+        pSpam = sum(trainCategory) / float(numTrainDoc)
+        wordsInSpamNum = np.ones(numWords)
+        wordsInHealthNum = np.ones(numWords)
+        SpamWordsNum = 2.0
+        HealthWordsNum = 2.0
+        for i in range(0, numTrainDoc):
+            if trainCategory[i] == 1:
+                wordsInSpamNum += trainMarkedWords[i]
+                SpamWordsNum += sum(trainMarkedWords[i])  # 统计Spam中语料库中词汇出现的总次数
+            if trainCategory[i] == 0:
+                wordsInHealthNum += trainMarkedWords[i]
+                HealthWordsNum += sum(trainMarkedWords[i])
+
+        pWordsSpamicity = np.log(wordsInSpamNum / SpamWordsNum)
+        pWordsHealthy = np.log(wordsInHealthNum / HealthWordsNum)
+
+        self.pWordsHealthy_, self.pWordsSpamicity_, self.pSpam_ = pWordsHealthy, pWordsSpamicity, pSpam
+        return self
+
+
+
+    def predict(self, vocabularyList, testWords):
+        testWordsCount = self.setOfWordsToVecTor(vocabularyList, testWords)
+        testWordsMarkedArray = np.array(testWordsCount)
+        # 计算P(Ci|W)，W为向量。P(Ci|W)只需计算P(W|Ci)P(Ci)
+        p1 = sum(testWordsMarkedArray * self.pWordsSpamicity_) + np.log(self.pSpam_)
+        p0 = sum(testWordsMarkedArray * self.pWordsHealthy_) + np.log(1 - self.pSpam_)
+        if p1 > p0:
+            return 1
+        else:
+            return 0
--- test/test_NaiveBayes.py
+++ test/test_NaiveBayes.py
@@ -5,17 +5,111 @@
 @File:   test_NaiveBayes.py
 """
 
-import re
+import numpy as np
+import sys
+import random
+sys.path.append('..')
+from SupervisedLearning.NaiveBayes import NaiveBayes
 
-def textParse(text):
-    text_ = re.split(r'\W*', text)
-    return [tok.lower() for tok in text if len(tok) > 2]
+def textParser(text):
+    import re
+    regEx = re.compile(r'[^a-zA-Z]|\d')
+    words = regEx.split(text)
+    words = [word.lower() for word in words if len(word) > 0]
+    return words
 
 
-docList = []
-classList = []
-fullText = []
+def loadSMSData(fileName):
+    f = open(fileName)
+    classCategory = []  # 类别标签，1表示是垃圾SMS，0表示正常SMS
+    smsWords = []
+    for line in f.readlines():
+        linedatas = line.strip().split('\t')
+        if linedatas[0] == 'ham':
+            classCategory.append(0)
+        elif linedatas[0] == 'spam':
+            classCategory.append(1)
+        # 切分文本
+        words = textParser(linedatas[1])
+        smsWords.append(words)
+    return smsWords, classCategory
 
-for i in range(1, 26):
-    wordList = open('../Data/email4bayes/spam/%d.txt' % i, 'r').read()
-    print(wordList)
+
+def createVocabularyList(smsWords):
+    vocabularySet = set([])
+    for words in smsWords:
+        vocabularySet = vocabularySet | set(words)
+    vocabularyList = list(vocabularySet)
+    return vocabularyList
+
+
+def getVocabularyList(fileName):
+    fr = open(fileName)
+    vocabularyList = fr.readline().strip().split('\t')
+    fr.close()
+    return vocabularyList
+
+
+def setOfWordsToVecTor(vocabularyList, smsWords):
+    vocabMarked = [0] * len(vocabularyList)
+    for smsWord in smsWords:
+        if smsWord in vocabularyList:
+            vocabMarked[vocabularyList.index(smsWord)] += 1
+    return vocabMarked
+
+
+def setOfWordsListToVecTor(vocabularyList, smsWordsList):
+    vocabMarkedList = []
+    for i in range(len(smsWordsList)):
+        vocabMarked = setOfWordsToVecTor(vocabularyList, smsWordsList[i])
+        vocabMarkedList.append(vocabMarked)
+    return vocabMarkedList
+
+
+# 导入数据
+filename = '../Data/emails/training/SMSCollection.txt'
+smsWords, classLables = loadSMSData(filename)
+
+# 交叉验证
+testWords = []
+testWordsType = []
+
+testCount = 1000
+for i in range(testCount):
+    randomIndex = int(random.uniform(0, len(smsWords)))
+    testWordsType.append(classLables[randomIndex])
+    testWords.append(smsWords[randomIndex])
+    del (smsWords[randomIndex])
+    del (classLables[randomIndex])
+
+vocabularyList = createVocabularyList(smsWords)
+print("生成语料库！")
+trainMarkedWords = setOfWordsListToVecTor(vocabularyList, smsWords)
+print("数据标记完成！")
+# 转成array向量
+trainMarkedWords = np.array(trainMarkedWords)
+print( "数据转成矩阵！")
+
+
+# 训练模型
+naiveBayes = NaiveBayes()
+naiveBayes.fit(trainMarkedWords, classLables)
+
+errorCount = 0.0
+for i in range(testCount):
+    smsType = naiveBayes.predict(vocabularyList, testWords[i])
+    print('预测类别：', smsType, '实际类别：', testWordsType[i])
+    if smsType != testWordsType[i]:
+        errorCount += 1
+
+print('错误个数：', int(errorCount), '错误率：', errorCount / testCount)
+
+
+# 加载测试数据
+print('加载测试数据......')
+filename = '../Data/emails/test/test.txt'
+smsWords_test, classLables_test = loadSMSData(filename)
+
+smsType = naiveBayes.predict(vocabularyList,smsWords_test[0])
+print('测试数据')
+print('预测类别：', smsType, '实际类别：', classLables_test[0])
